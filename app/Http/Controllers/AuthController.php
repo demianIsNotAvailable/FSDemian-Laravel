3d96343c79d806514af8db24f07d05f71e26aae1
Dani <d.tarazoma@geekshubs.com>
2023-05-31 08:47:03 +0200

    feat: register users done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+
+    const ROLE_USER = 1;
+
     public function register(Request $request)
     {
         try {
@@ -27,18 +32,42 @@ class AuthController extends Controller
                 );
             }
 
+            // recuperamos la info 
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $password = $request->input('password');
+
+            // tratamos la info
+            $encryptedPassword = bcrypt($password);
+            $roleId = self::ROLE_USER;
+
+            // guardamos en BD
+            $user = User::create([
+                "name" => $name,
+                "email" => $email,
+                "password" => $encryptedPassword,
+                "role_id" => $roleId
+            ]);
+
+            // creamos el token del usuario
+            $token = $user->createToken('apiToken')->plainTextToken;
+
             return response()->json(
                 [
                     "success" => true,
-                    "message" => "User registered"
+                    "message" => "User registered",
+                    "data" => $user,
+                    "token" => $token
                 ],
                 201
             );
         } catch (\Throwable $th) {
+            Log::error("Error registering user: ". $th->getMessage());
             return response()->json(
                 [
                     "success" => false,
-                    "message" => "User cant be registered"
+                    "message" => "User cant be registered",
+                    "error" => $th->getMessage()
                 ],
                 500
             );
